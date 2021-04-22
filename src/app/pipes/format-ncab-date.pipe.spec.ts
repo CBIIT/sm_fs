0b10c7ee78d438fb029216db7d5641fed7ccbcbc
Bin Li <bin.li@itgfirm.com>
2021-04-22 18:42:06 -0400

    FS-22

--- src/app/pipes/format-ncab-date.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FormatNcabDatePipe } from './format-ncab-date.pipe';
-
-describe('FormatNcabDatePipe', () => {
-  it('create an instance', () => {
-    const pipe = new FormatNcabDatePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
