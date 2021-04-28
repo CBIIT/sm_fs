37df446e05734b020f5d2742a9023531974b8ef9
Esparham <esparhams@nih.gov>
2021-04-28 10:53:29 -0400

    FS-4

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,71 +1,78 @@
-import {Component, OnInit, AfterViewChecked} from '@angular/core';
-import {AppNavigationTControllerService, NciPerson} from '@nci-cbiit/i2ecws-lib';
-import {UserService} from '@nci-cbiit/i2ecui-lib';
-import {environment} from '../../environments/environment';
-import {AppPropertiesService} from '../service/app-properties.service';
+import { Component, OnInit, AfterViewChecked } from '@angular/core';
+import { AppNavigationTControllerService, NciPerson } from '@nci-cbiit/i2ecws-lib';
+import { UserService } from '@nci-cbiit/i2ecui-lib';
+import { environment } from '../../environments/environment';
+import { AppPropertiesService } from '../service/app-properties.service';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css'],
+	selector: 'app-header',
+	templateUrl: './header.component.html',
+	styleUrls: ['./header.component.css'],
 })
+
 export class HeaderComponent implements OnInit {
 
-  loggedOnUser: NciPerson;
-  nciHome: string;
-  workBenchUrl: string;
-  nearUrl: string;
-  canChangeUser: boolean;
+	loggedOnUser: NciPerson;
+	nciHome: string;
+	workBenchUrl: string;
+	nearUrl: string;
+	canChangeUser: boolean;
 
-  mainMenus = [];
-  otherMenus = [];
+	public headerTemplateData = {
+		appName: "Funding Selection",
+		docs: [{ docName: "Example Doc1", docLink: "" }],
+		helpGuides: [{ helpGuideName: "Example Link", helpGuideLink: "" }]
+	}
 
-  constructor(private userService: UserService,
-              private appNavService: AppNavigationTControllerService,
-              private appPropertiesService: AppPropertiesService) {
-  }
+	mainMenus = [];
+	otherMenus = [];
 
-  ngOnInit(): void {
-    this.userService.getSecurityCredentials().subscribe(
-      result => {
-        if (result) {
-          console.log(result);
-          this.loggedOnUser = result.nciPerson;
-          this.canChangeUser = this.userService.isTechSupportAuth(result.authorities);
-          let roles = [];
-          result.authorities.forEach(authority => {
-            roles.push(authority.authority);
-          });
-          let linked = [];
-          this.appNavService.getNavLinksUsingGET(roles).subscribe(
-            result2 => {
-              if (result2) {
-                result2.forEach(appNav => {
-                  if (linked.indexOf(appNav.gwbLinksTDto.displayName) > -1) {
-                    return;
-                  }
-                  linked.push(appNav.gwbLinksTDto.displayName);
-                  let nav: { name: string, url: string } = {name: '', url: ''};
-                  nav.url = appNav.gwbLinksTDto.composedUrl;
-                  nav.name = appNav.gwbLinksTDto.displayName;
-                  if (appNav.mainDisplayFlag === 'Y') {
-                    this.mainMenus.push(nav);
-                  } else {
-                    this.otherMenus.push(nav);
-                  }
+	constructor(private userService: UserService,
+		private appNavService: AppNavigationTControllerService,
+		private appPropertiesService: AppPropertiesService) {
+	}
 
-                });
-                this.mainMenus.push({name: 'FS', url: '/', target: ''});
-                console.log('main nav menus', this.mainMenus);
-                console.log('other nav menus', this.otherMenus);
-              }
-            });
-        }
-      });
+	ngOnInit(): void {
+		this.userService.getSecurityCredentials().subscribe(
+			result => {
+				if (result) {
+					console.log(result);
+					this.loggedOnUser = result.nciPerson;
+					this.canChangeUser = this.userService.isTechSupportAuth(result.authorities);
+					let roles = [];
+					result.authorities.forEach(authority => {
+						roles.push(authority.authority);
+					});
+					let linked = [];
+					this.appNavService.getNavLinksUsingGET(roles).subscribe(
+						result2 => {
+							if (result2) {
+								result2.forEach(appNav => {
+									if (linked.indexOf(appNav.gwbLinksTDto.displayName) > -1) {
+										return;
+									}
+									linked.push(appNav.gwbLinksTDto.displayName);
+									let nav: { name: string, url: string } = { name: '', url: '' };
+									nav.url = appNav.gwbLinksTDto.composedUrl;
+									nav.name = appNav.gwbLinksTDto.displayName;
+									if (appNav.mainDisplayFlag === 'Y') {
+										this.mainMenus.push(nav);
+									} else {
+										this.otherMenus.push(nav);
+									}
 
-    this.workBenchUrl = this.appPropertiesService.getProperty('workBenchUrl');
-    this.nciHome = this.appPropertiesService.getProperty('nciHome');
-    this.nearUrl = this.appPropertiesService.getProperty('nearUrl');
+								});
+								this.mainMenus.push({ name: 'FS', url: '/', target: '' });
+								console.log('main nav menus', this.mainMenus);
+								console.log('other nav menus', this.otherMenus);
+							}
+						});
+				}
+			});
 
-  }
+		this.workBenchUrl = this.appPropertiesService.getProperty('workBenchUrl');
+		this.nciHome = this.appPropertiesService.getProperty('nciHome');
+		this.nearUrl = this.appPropertiesService.getProperty('nearUrl');
+
+	}
 }
