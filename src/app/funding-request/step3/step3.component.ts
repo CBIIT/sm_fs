845a2a4276fccc96bbaa88972d35e05791a0fa3f
Prakash Garapati <prakash.garapati@nih.gov>
2021-04-28 15:27:28 -0400

    FS-26 - Upload Docs for Funding Request

--- src/app/funding-request/step3/step3.component.ts
+++ src/app/funding-request/step3/step3.component.ts
@@ -1,5 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input , Output , EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
+import 'select2';
+import { Select2OptionData} from 'ng-select2';
+import { Options } from 'select2';
+import { CgRefCodControllerService, CgRefCodesDto } from '@nci-cbiit/i2ecws-lib';
+import { DocumentService } from '../../service/document.service';
+import { Observable } from 'rxjs';
+import { HttpEventType, HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-step3',
@@ -8,9 +15,71 @@ import { Router } from '@angular/router';
 })
 export class Step3Component implements OnInit {
 
-  constructor(private router:Router) { }
+  @Input() apiMethodName: string;
+  @Output() selectedDocTypeValue = new EventEmitter<string>();
+  public DocTypes:  Array<CgRefCodesDto>;
+  public options : Options;
+
+  selectedFiles: FileList;
+  progressInfos = [];
+  message = '';
+
+  fileInfos: Observable<any>;
+
+
+  constructor(private router:Router,
+    private cgRefCodControllerService:CgRefCodControllerService,
+    private documentService: DocumentService) { }
+
+    // For getting the I2 status selected value and emitting the value..
+  public _selectedDocType: string = '';
+
+  set selectedDocType(selectedDocType: string) {
+    this._selectedDocType = selectedDocType;
+    console.log(selectedDocType);
+    this.selectedDocTypeValue.emit(this._selectedDocType);
+  }
+
+  selectFiles(event) {
+    this.progressInfos = [];
+    this.selectedFiles = event.target.files;
+  }
+
+  uploadFiles() {
+    this.message = '';
+  
+    for (let i = 0; i < this.selectedFiles.length; i++) {
+      this.upload(i, this.selectedFiles[i]);
+    }
+  }
+
+  upload(idx, file) {
+    this.progressInfos[idx] = { value: 0, fileName: file.name };
+  
+    this.documentService.upload(file).subscribe(
+      event => {
+        // if (event.type === HttpEventType.UploadProgress) {
+        //   this.progressInfos[idx].value = Math.round(100 * event.loaded / event.total);
+        // } else if (event instanceof HttpResponse) {
+        //   this.fileInfos = this.documentService.getFiles();
+        // }
+      },
+      err => {
+        this.progressInfos[idx].value = 0;
+        this.message = 'Could not upload the file:' + file.name;
+      });
+  }
 
   ngOnInit(): void {
+
+    this.cgRefCodControllerService.getPfrDocTypeUsingGET().subscribe(
+      result => {
+        console.log('Getting the Doc Dropdown results');
+        this.DocTypes = result;
+      },error => {
+        console.log( 'HttpClient get request error for----- '+ error.message);
+      });
+
   }
 
   nextStep() {
---
+++ src/app/service/document.service.ts
@@ -0,0 +1,71 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpRequest, HttpHeaders, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { DocumentsControllerService } from '@nci-cbiit/i2ecws-lib';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DocumentService {
+
+  private baseUrl = 'http://localhost:8080';
+  private response: String;
+  fileString:string = "";
+ // fileString: string;
+  
+  
+  constructor(private http: HttpClient,
+    private documentsControllerService: DocumentsControllerService) { }
+
+  upload(file: File): Observable<string> {
+    const formData: FormData = new FormData();
+
+    formData.append('file', file);
+
+    // const req = new HttpRequest('POST', `${this.baseUrl}/upload`, formData, {
+    //   reportProgress: true,
+    //   responseType: 'json'
+    // });
+
+    
+    var reader = new FileReader();
+
+  reader.onload = function () {
+    console.log(reader.result);
+  }
+
+
+  
+
+  reader.readAsBinaryString(file);
+
+  reader.onloadend = (e) => {
+    //console.log(myReader.result);
+    // Entire file
+    console.log(reader.result);
+
+    // By lines
+    
+
+    this.fileString = reader.result as string;
+ };
+
+
+    return this.documentsControllerService.addDocumentUsingPOST(this.fileString, 1, "abc", "test").pipe(
+
+      map(
+        result => {
+          this.response = result;
+          return result;
+        })
+
+      
+
+      );
+    }
+
+  getFiles(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/files`);
+  }
+}
