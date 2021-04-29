5cb63f641d2b28bda05b3a980a7df3b20bed3934
Prakash Garapati <prakash.garapati@nih.gov>
2021-04-29 16:47:31 -0400

    FS-26 - Upload Docs for Funding Request

--- src/app/service/document.service.ts
+++ src/app/service/document.service.ts
@@ -1,71 +1,33 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpRequest, HttpHeaders, HttpEvent } from '@angular/common/http';
+import { HttpClient, HttpRequest, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { DocumentsControllerService } from '@nci-cbiit/i2ecws-lib';
-import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DocumentService {
 
-  private baseUrl = 'http://localhost:8080';
-  private response: String;
-  fileString:string = "";
- // fileString: string;
+  private docUrl = 'i2ecws/api/v1/documents';
 
+  constructor(private http: HttpClient) { }
 
-  constructor(private http: HttpClient,
-    private documentsControllerService: DocumentsControllerService) { }
-
-  upload(file: File): Observable<string> {
+  upload(file: File): Observable<HttpEvent<any>> {
     const formData: FormData = new FormData();
 
     formData.append('file', file);
+    formData.append('keyId', '1');
+    formData.append('keyType', 'PFR');
+    formData.append('docType', 'Doc Type');
 
-    // const req = new HttpRequest('POST', `${this.baseUrl}/upload`, formData, {
-    //   reportProgress: true,
-    //   responseType: 'json'
-    // });
+    const req = new HttpRequest('POST', `${this.docUrl}/add`, formData, {
+      reportProgress: true,
+      responseType: 'json'
+    });
+    return this.http.request(req);
 
-
-    var reader = new FileReader();
-
-  reader.onload = function () {
-    console.log(reader.result);
   }
 
-
-
-
-  reader.readAsBinaryString(file);
-
-  reader.onloadend = (e) => {
-    //console.log(myReader.result);
-    // Entire file
-    console.log(reader.result);
-
-    // By lines
-
-
-    this.fileString = reader.result as string;
- };
-
-
-    return this.documentsControllerService.addDocumentUsingPOST(this.fileString, 1, "abc", "test").pipe(
-
-      map(
-        result => {
-          this.response = result;
-          return result;
-        })
-
-
-
-      );
-    }
-
   getFiles(): Observable<any> {
-    return this.http.get(`${this.baseUrl}/files`);
+    return this.http.get(`${this.docUrl}/files`);
   }
 }
