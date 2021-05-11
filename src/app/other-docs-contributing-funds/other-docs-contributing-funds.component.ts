a512d67a7fbec4422b630987cfdbe8ed7e7cd177
kanozad <doug.kanoza@gmail.com>
2021-05-11 10:14:13 -0400

    FS-4 Creating stub for custom DOCs component to manage drag and drop ordering

--- src/app/other-docs-contributing-funds/other-docs-contributing-funds.component.ts
+++ src/app/other-docs-contributing-funds/other-docs-contributing-funds.component.ts
@@ -1,6 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {RequestModel} from '../model/request-model';
-import {NciPfrGrantQueryDto} from '@nci-cbiit/i2ecws-lib';
+import {LookupsControllerService, NciPfrGrantQueryDto} from '@nci-cbiit/i2ecws-lib';
+import {Select2OptionData} from 'ng-select2';
+import {Options} from 'select2';
 
 @Component({
   selector: 'app-other-docs-contributing-funds',
@@ -9,17 +11,51 @@ import {NciPfrGrantQueryDto} from '@nci-cbiit/i2ecws-lib';
 })
 export class OtherDocsContributingFundsComponent implements OnInit {
 
-  constructor(private requestModel: RequestModel) {
+
+  @Input() maxSelection = 1;
+  @Input() label = 'Division/Office/Center (DOC)';
+
+  @Input()
+  get selectedValue(): number[] {
+    return this._selectedValue;
+  }
+
+  @Output() selectedValueChange = new EventEmitter<number[]>();
+
+  set selectedValue(value: number[]) {
+    console.log('DOC selectedValue setter called ', value);
+    this._selectedValue = value;
+    this.selectedValueChange.emit(value);
+  }
+
+  private _selectedValue: number [] = [];
+
+  // NOTE: if the following array is typed as Select2OptionData, compilation fails
+  public docs: Array<any>;
+  public options: Options;
+
+  constructor(private lookupsControllerService: LookupsControllerService, private requestModel: RequestModel) {
   }
 
   ngOnInit(): void {
+    this.options = {};
+    const multi = (Number(this.maxSelection) !== Number(1));
+    console.log('Multiple select:', multi);
+    if (multi) {
+      this.options.multiple = true;
+    }
+    if (multi && this.maxSelection !== -1) {
+      this.options.maximumSelectionLength = this.maxSelection;
+    }
+    this.lookupsControllerService.getNciDocsUsingGET().subscribe(
+      result => {
+        console.log('Getting the Doc Dropdown results');
+        // result.push({id: '', abbreviation: '', description: ''});
+        this.docs = result;
+      }, error => {
+        console.log('HttpClient get request error for----- ' + error.message);
+      });
+
   }
 
-  get grant(): NciPfrGrantQueryDto {
-    return this.requestModel.grant;
-  }
-
-  get model(): RequestModel {
-    return this.requestModel;
-  }
 }
