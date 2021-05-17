9f4f8e0fb85ce4906b254e0ed1e8bfb87586c094
Koneru <manoja.koneru@nih.gov>
2021-05-17 13:40:04 -0400

    FS-11 Funding Source tooltip url issue

--- src/app/program-recommended-costs/program-recommended-costs.component.ts
+++ src/app/program-recommended-costs/program-recommended-costs.component.ts
@@ -37,10 +37,10 @@ export class ProgramRecommendedCostsComponent implements OnInit {
 
   // open the funding source help in the new window..
   openFsDetails(){
-    const url = this.router.serializeUrl(
-      this.router.createUrlTree([`/fundingSourceDetails`])
-    );
-  
+    //const url = this.router.serializeUrl(
+      //this.router.createUrlTree([`/fundingSourceDetails`])
+    //);
+    const url = "/fs"+this.router.createUrlTree(['fundingSourceDetails']).toString();
     openNewWindow(url,'fundingSourceDetails');
     return false;
   }
