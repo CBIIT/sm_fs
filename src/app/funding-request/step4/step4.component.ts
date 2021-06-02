5984aeceb13f9599ecef81c42ab392ea4c8abd38
Esparham <esparhams@nih.gov>
2021-06-02 14:57:56 -0400

    Added logger info

--- src/app/funding-request/step4/step4.component.ts
+++ src/app/funding-request/step4/step4.component.ts
@@ -1,16 +1,19 @@
-import {ChangeDetectorRef, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
-import {Router} from '@angular/router';
-import {RequestModel} from '../../model/request-model';
-import {AppPropertiesService} from '../../service/app-properties.service';
-import {FsRequestControllerService, FundingReqStatusHistoryDto,
+import { ChangeDetectorRef, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { RequestModel } from '../../model/request-model';
+import { AppPropertiesService } from '../../service/app-properties.service';
+import {
+  FsRequestControllerService, FundingReqStatusHistoryDto,
   NciPfrGrantQueryDto, FundingRequestDtoReq, DocumentsDto,
-  FsWorkflowControllerService} from '@nci-cbiit/i2ecws-lib';
-import {AppUserSessionService} from 'src/app/service/app-user-session.service';
-import {FundingRequestIntegrationService} from '../integration/integration.service';
-import {Subscription} from 'rxjs';
+  FsWorkflowControllerService
+} from '@nci-cbiit/i2ecws-lib';
+import { AppUserSessionService } from 'src/app/service/app-user-session.service';
+import { FundingRequestIntegrationService } from '../integration/integration.service';
+import { Subscription } from 'rxjs';
 import { DocumentService } from '../../service/document.service';
 import { saveAs } from 'file-saver';
 import { HttpResponse } from '@angular/common/http';
+import { NGXLogger } from 'ngx-logger';
 
 @Component({
   selector: 'app-step4',
@@ -24,20 +27,21 @@ export class Step4Component implements OnInit, OnDestroy {
   grantViewerUrl: string = this.propertiesService.getProperty('GRANT_VIEWER_URL');
   isRequestEverSubmitted = false;
   requestHistorySubscriber: Subscription;
-  submissionResult = {frqId: null, approver: null};
+  submissionResult = { frqId: null, approver: null };
   dv = true;
   requestStatus: string;
   docDtos: DocumentsDto[];
 
   constructor(private router: Router,
-              private requestModel: RequestModel,
-              private propertiesService: AppPropertiesService,
-              private fsRequestService: FsRequestControllerService,
-              private fsWorkflowService: FsWorkflowControllerService,
-              private userSessionService: AppUserSessionService,
-              private requestIntegrationService: FundingRequestIntegrationService,
-              private documentService: DocumentService,
-              private changeDetection: ChangeDetectorRef) {
+    private requestModel: RequestModel,
+    private propertiesService: AppPropertiesService,
+    private fsRequestService: FsRequestControllerService,
+    private fsWorkflowService: FsWorkflowControllerService,
+    private userSessionService: AppUserSessionService,
+    private requestIntegrationService: FundingRequestIntegrationService,
+    private documentService: DocumentService,
+    private changeDetection: ChangeDetectorRef,
+    private logger: NGXLogger) {
   }
 
   ngOnDestroy(): void {
@@ -47,7 +51,7 @@ export class Step4Component implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
-    console.log('Step4 requestModel ', this.requestModel);
+    this.logger.debug('Step4 requestModel: ', this.requestModel);
     this.requestModel.setStepLinkable(4, true);
     this.requestHistorySubscriber = this.requestIntegrationService.requestHistoryLoadEmitter.subscribe(
       (historyResult) => {
@@ -85,15 +89,13 @@ export class Step4Component implements OnInit, OnDestroy {
   parseRequestHistories(historyResult: FundingReqStatusHistoryDto[]): void {
     let submitted = false;
     historyResult.forEach((item: FundingReqStatusHistoryDto) => {
-      console.log('inside loop of parseRequestHistories ', item);
-      // const i = item.statusDescrip.indexOf(' by ');
+      this.logger.debug('Parse Request History: ', item);
       const i = item.statusDescrip.search(/ by /gi);
       if (i > 0) {
         item.statusDescrip = item.statusDescrip.substring(0, i);
       }
 
       if (item.statusCode === 'SUBMITTED') {
-        console.log('isRequestEverSubmitted=true');
         submitted = true;
       }
 
@@ -120,14 +122,14 @@ export class Step4Component implements OnInit, OnDestroy {
 
   deleteRequest(): void {
     if (confirm('Are you sure you want to delete this request?')) {
-      console.log('Call deleteRequest API for frqId ', this.model.requestDto.frqId);
+      this.logger.debug('Call deleteRequest API for FRQ ID: ', this.model.requestDto.frqId);
       this.fsRequestService.deleteRequestUsingDELETE(this.model.requestDto.frqId).subscribe(
         result => {
-          console.log('Call API to delete completed route to /search, API returned ', result);
+          this.logger.debug('Funding request was deleted: ', result);
           this.router.navigate(['/search']);
         },
         error => {
-          console.log('Error when calling delelteRequest API ', error);
+          this.logger.error('Error when calling delelteRequest API ', error);
         }
       );
     }
@@ -135,29 +137,30 @@ export class Step4Component implements OnInit, OnDestroy {
 
   submitRequest(): void {
     const submissionDto: FundingRequestDtoReq = {};
+
     // submitRequest DAO method only needs following parameters
     submissionDto.frqId = this.requestModel.requestDto.frqId;
     submissionDto.requestorNpeId = this.requestModel.requestDto.requestorNpeId;
     submissionDto.certCode = this.requestModel.requestDto.certCode;
     submissionDto.comments = this.requestModel.requestDto.comments;
-    console.log('submitRquest, requestDto=', submissionDto);
+    this.logger.debug('Submit Request for: ', submissionDto);
     this.fsRequestService.submitRequestUsingPOST(submissionDto).subscribe(
       (result) => {
-        console.log('calling submitRequestUsingPost successful, it returns', result);
-        this.submissionResult = {frqId: submissionDto.frqId, approver: 'Mr. Approver'};
+        this.logger.debug('Submit Request result: ', result);
+        this.submissionResult = { frqId: submissionDto.frqId, approver: 'Mr. Approver' };
         this.requestIntegrationService.requestSubmissionEmitter.next(submissionDto.frqId);
         this.submitSuccess.nativeElement.scrollIntoView();
       },
       (error) => {
-        console.log('Failed when calling submitRequestUsingPOST', error);
+        this.logger.error('Failed when calling submitRequestUsingPOST', error);
       });
   }
 
   userCanSubmitAndDelete(): boolean {
     if (this.userSessionService.isPD())
-      // need to add checks whether loggedOn user is the requesting pd or if the logged on user's CA
-      // is the same as the requesting pd's CA && this.userSessionService.getLoggedOnUser().npnId === this.model.requestDto.requestorNpnId)
-      // TODO: Bin, I have put validation logic on the request model for saving a request.  You could also put submit validation there.
+    // need to add checks whether loggedOn user is the requesting pd or if the logged on user's CA
+    // is the same as the requesting pd's CA && this.userSessionService.getLoggedOnUser().npnId === this.model.requestDto.requestorNpnId)
+    // TODO: Bin, I have put validation logic on the request model for saving a request.  You could also put submit validation there.
     {
       return true;
     } else {
@@ -204,19 +207,19 @@ export class Step4Component implements OnInit, OnDestroy {
       this.downloadSummaryStatement();
     } else {
       this.documentService.downloadById(id)
-      .subscribe(
-        (response: HttpResponse<Blob>) => {
-          let blob = new Blob([response.body], { 'type': response.headers.get('content-type') });
-          saveAs(blob, fileName)
-        }
-      )
+        .subscribe(
+          (response: HttpResponse<Blob>) => {
+            let blob = new Blob([response.body], { 'type': response.headers.get('content-type') });
+            saveAs(blob, fileName)
+          }
+        )
     }
-   
+
   }
 
   downloadSummaryStatement() {
     this.documentService.downloadFrqSummaryStatement(this.requestModel.grant.applId).subscribe(blob => saveAs(blob, 'Summary Statement.pdf')), error =>
-      console.log('Error downloading the file'),
+      this.logger.error('Error downloading the file'),
       () => console.info('File downloaded successfully');
   }
 
