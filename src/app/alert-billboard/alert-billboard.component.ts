34f38a551e0c034a1c748cecd49a7e56ac1032ba
kanozad <doug.kanoza@gmail.com>
2021-06-15 07:16:00 -0400

    Validation Interim commit

---
+++ src/app/alert-billboard/alert-billboard.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Alert} from '../service/alert';
+import {AlertService} from '../service/alert.service';
+
+@Component({
+  selector: 'app-alert-billboard',
+  templateUrl: './alert-billboard.component.html',
+  styleUrls: ['./alert-billboard.component.css']
+})
+export class AlertBillboardComponent implements OnInit {
+  @Input() alert: Alert;
+
+  constructor(private alertService: AlertService) {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
