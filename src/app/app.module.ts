b208e8c5b2c738fa2a9645708713ce65c2996789
Bin Li <bin.li@itgfirm.com>
2021-06-22 12:25:56 -0400

    FS-448 FS-309

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -61,6 +61,7 @@ import {Type4ConversionMechanismComponent} from './type4-conversion-mechanism/ty
 import {AlertBillboardComponent} from './alert-billboard/alert-billboard.component';
 import {OpenRequestComponent} from './search/open-request/open-request.component';
 import { WorkflowComponent } from './funding-request/workflow/workflow.component';
+import { ApproverListsComponent } from './funding-request/workflow/approver-lists/approver-lists.component';
 
 export function initializeAppProperties(appPropertiesService: AppPropertiesService): any {
   return (): Promise<any> => {
@@ -131,6 +132,7 @@ export function initializeGwbLinks(gwbLinksService: GwbLinksService): any {
     AlertBillboardComponent,
     OpenRequestComponent,
     WorkflowComponent,
+    ApproverListsComponent,
   ],
 
 
