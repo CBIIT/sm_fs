a0d238b24b792101336c9014caa36de0f95119fc
Esparham <esparhams@nih.gov>
2021-07-08 15:29:04 -0400

    FS-500

--- src/app/request-information-readonly/request-information-readonly.component.ts
+++ src/app/request-information-readonly/request-information-readonly.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import {RequestModel} from '../model/request-model';
-import {NciPfrGrantQueryDto} from '@nci-cbiit/i2ecws-lib';
+import { RequestModel } from '../model/request-model';
+import { NciPfrGrantQueryDto } from '@nci-cbiit/i2ecws-lib';
+
+let loaMap = new Map([
+  ["PD", "PD"],
+  ["DD", "NCI Director or Designeee"],
+  ["SPL", "Scientific Program Leaders Committee (SPL)"],
+  ["DAO", "Division/Office/Center Approver"]
+]);
 
 @Component({
   selector: 'app-request-information-readonly',
   templateUrl: './request-information-readonly.component.html',
   styleUrls: ['./request-information-readonly.component.css']
 })
+
 export class RequestInformationReadonlyComponent implements OnInit {
 
   constructor(private requestModel: RequestModel) { }
@@ -22,4 +30,7 @@ export class RequestInformationReadonlyComponent implements OnInit {
     return this.requestModel;
   }
 
+  get loaMap(): typeof loaMap {
+    return this.loaMap;
+  }
 }
