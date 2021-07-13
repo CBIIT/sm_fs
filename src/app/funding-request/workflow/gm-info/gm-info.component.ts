ec80c15d278fa2aa4cc7be816bb5ea018152dabf
Bin Li <bin.li@itgfirm.com>
2021-07-13 12:36:13 -0400

    FS-127

--- src/app/funding-request/workflow/gm-info/gm-info.component.ts
+++ src/app/funding-request/workflow/gm-info/gm-info.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FsLookupControllerService, FsWorkflowControllerService, GmInfoDto } from '@nci-cbiit/i2ecws-lib';
+import { NGXLogger } from 'ngx-logger';
+import { RequestModel } from 'src/app/model/request/request-model';
+import { WorkflowModel } from '../workflow.model';
 
 @Component({
   selector: 'app-gm-info',
@@ -6,14 +10,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gm-info.component.css']
 })
 export class GmInfoComponent implements OnInit {
+  @Input() approvingState = false;
 
-  model: any;
+  options: any = {};
+  gmInfo: GmInfoDto = {};
+  gmSpecialistMap: Map<number, any> = new Map<number, any>();
+  bkupSpecList: {id: string, text: string}[];
+  defaultSpecList: string[];
 
-  options: any;
-
-  constructor() { }
+  constructor(private workflowService: FsWorkflowControllerService,
+              private requestModel: RequestModel,
+              private workflowModel: WorkflowModel,
+              private lookupService: FsLookupControllerService,
+              private logger: NGXLogger) { }
 
   ngOnInit(): void {
+    this.loadData();
+  }
+
+  loadData(): void {
+      this.workflowService.getDefaultGmInfoUsingGET(this.requestModel.requestDto.frqId).subscribe(
+      result => {
+        if (result) {
+          this.gmInfo = result;
+          this.logger.debug('getDefaultGmInfoUsingGET returned', result);
+        }
+        else {
+          this.gmInfo = {};
+        }
+        this.gmInfo.frqId = this.requestModel.requestDto.frqId;
+        this.gmInfo.actionType = this.requestModel.requestDto.defaultActionType;
+      },
+      error => {
+        this.logger.error('getDefaultGmInfoUsingGET failed', error);
+      }
+    );
+      this.lookupService.getGmActiveSpecialistsUsingGET().subscribe(
+      result => {
+        if (result) {
+          this.logger.debug('getGmActiveSpecialistsUsingGET returned', result);
+          this.bkupSpecList = result.map( (data) => ({id: data.specNpeId, text: data.specFullName}));
+          this.defaultSpecList = result.map( (data) => (data.specFullName));
+        }
+        this.gmInfo.frqId = this.requestModel.requestDto.frqId;
+        this.gmInfo.actionType = this.requestModel.requestDto.defaultActionType;
+      },
+      error => {
+        this.logger.error('getGmActiveSpecialistsUsingGET failed', error);
+      }
+    );
+  }
+
+  getGmInfo(): GmInfoDto {
+    return this.gmInfo;
+  }
+
+  get editable(): boolean {
+    return this.workflowModel.isScientificApprover && this.approvingState;
   }
 
 }
