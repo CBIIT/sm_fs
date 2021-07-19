24c3bcb1f1fa603cfe08843d9df997ac10917052
Doug Kanoza <kanozad@users.noreply.github.com>
2021-07-19 14:10:15 -0400

    adding grant lists for Prakash

--- src/app/model/plan/plan-model.ts
+++ src/app/model/plan/plan-model.ts
@@ -1,19 +1,23 @@
-import {Injectable} from '@angular/core';
-import {NciPfrGrantQueryDto} from '@nci-cbiit/i2ecws-lib';
-import {AppPropertiesService} from '../../service/app-properties.service';
+import { Injectable } from '@angular/core';
+import { NciPfrGrantQueryDto } from '@nci-cbiit/i2ecws-lib';
+import { AppPropertiesService } from '../../service/app-properties.service';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class PlanModel {
-    grantViewerUrl: string;
-    eGrantsUrl: string;
-    grants: NciPfrGrantQueryDto[] = [];
+  grantViewerUrl: string;
+  eGrantsUrl: string;
+  allgrants: NciPfrGrantQueryDto[] = [];
+  selectedGrants: NciPfrGrantQueryDto[] = [];
+  unselectedGrants: NciPfrGrantQueryDto[] = [];
+  skippedGrants: NciPfrGrantQueryDto[] = [];
+  exceptionGrants: NciPfrGrantQueryDto[] = [];
 
 
-    constructor(propertiesService: AppPropertiesService) {
-        this.grantViewerUrl = propertiesService.getProperty('GRANT_VIEWER_URL');
-        this.eGrantsUrl = propertiesService.getProperty('EGRANTS_URL');
+  constructor(propertiesService: AppPropertiesService) {
+    this.grantViewerUrl = propertiesService.getProperty('GRANT_VIEWER_URL');
+    this.eGrantsUrl = propertiesService.getProperty('EGRANTS_URL');
 
-    }
+  }
 }
