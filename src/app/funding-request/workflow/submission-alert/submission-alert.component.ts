d804c9ea146a11aad963c050fb75dbaa3d19ce66
Bin Li <bin.li@itgfirm.com>
2021-07-21 18:42:40 -0400

    FS-616

--- src/app/funding-request/workflow/submission-alert/submission-alert.component.ts
+++ src/app/funding-request/workflow/submission-alert/submission-alert.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FundingReqApproversDto } from '@nci-cbiit/i2ecws-lib';
 import { Subscription } from 'rxjs';
+import { RequestModel } from 'src/app/model/request/request-model';
 import { FundingRequestIntegrationService } from '../../integration/integration.service';
 import { WorkflowModel } from '../workflow.model';
 
@@ -19,6 +20,7 @@ export class SubmissionAlertComponent implements OnInit, OnDestroy {
   errorMessage: string;
 
   constructor(private workflowModel: WorkflowModel,
+              private requestModel: RequestModel,
               private integrationService: FundingRequestIntegrationService) {
   }
 
@@ -26,12 +28,16 @@ export class SubmissionAlertComponent implements OnInit, OnDestroy {
     if (this.requestSubmissionEventSubscriber) {
       this.requestSubmissionEventSubscriber.unsubscribe();
     }
+    if (this.requestSubmitFailureEventSubscriber) {
+      this.requestSubmitFailureEventSubscriber.unsubscribe();
+    }
   }
 
   ngOnInit(): void {
     this.requestSubmissionEventSubscriber = this.integrationService.requestSubmissionEmitter.subscribe(
       (dto) =>
         {
+          this.requestModel.clearAlerts();
           this.status = 'success';
           this.action = dto.completeRequest ? 'COMPLETE' : dto.action;
           this.frqId = dto.frqId;
@@ -40,6 +46,7 @@ export class SubmissionAlertComponent implements OnInit, OnDestroy {
     this.requestSubmitFailureEventSubscriber = this.integrationService.requestSubmitFailureEmitter.subscribe(
       (message) =>
         {
+          this.requestModel.clearAlerts();
           this.status = 'failure';
           this.errorMessage = message;
         }
