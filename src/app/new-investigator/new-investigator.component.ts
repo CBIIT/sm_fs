ab1da1d801c436c0b6fe603e9fc6d42ef5464a57
kanozad <doug.kanoza@gmail.com>
2021-08-03 16:02:05 -0400

    FS-604

--- src/app/new-investigator/new-investigator.component.ts
+++ src/app/new-investigator/new-investigator.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {RequestModel} from '../model/request/request-model';
-import {NGXLogger} from 'ngx-logger';
+import { RequestModel } from '../model/request/request-model';
+import { NGXLogger } from 'ngx-logger';
+import { Select2OptionData } from 'ng-select2';
 
 @Component({
   selector: 'app-new-investigator',
@@ -8,14 +9,21 @@ import {NGXLogger} from 'ngx-logger';
   styleUrls: ['./new-investigator.component.css']
 })
 export class NewInvestigatorComponent implements OnInit {
+  data: Select2OptionData[];
 
   get model(): RequestModel {
     return this.requestModel;
   }
 
-  constructor(private requestModel: RequestModel, private logger: NGXLogger) { }
+  constructor(private requestModel: RequestModel, private logger: NGXLogger) {
+  }
 
   ngOnInit(): void {
+    this.data = [
+      { id: '', text: '' },
+      { id: 'Y', text: 'Yes' },
+      { id: 'N', text: 'No' }
+    ];
   }
 
 }
