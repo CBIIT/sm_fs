6ed8b2e3a38de5dea82f14c452f76314a7c5dba5
Prakash Garapati <prakash.garapati@nih.gov>
2021-08-12 15:35:38 -0400

    FS-33 - Doc download and delete

--- src/app/funding-plan/plan-step5/plan-step5.component.ts
+++ src/app/funding-plan/plan-step5/plan-step5.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpResponse } from '@angular/common/http';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { DocumentsDto } from '@nci-cbiit/i2ecws-lib';
 import { NGXLogger } from 'ngx-logger';
-import { Observable, of } from 'rxjs';
 import { NavigationStepModel } from 'src/app/funding-request/step-indicator/navigation-step.model';
 import { PlanModel } from 'src/app/model/plan/plan-model';
 import { DocumentService } from 'src/app/service/document.service';
+import { saveAs } from 'file-saver';
 
 
 @Component({
@@ -18,7 +19,18 @@ export class PlanStep5Component implements OnInit {
   CR_FUNDING_PLAN_EXCEPTION_JUST_RPT = "CR_FUNDING_PLAN_EXCEPTION_JUST_RPT";
   CR_FUNDING_PLAN_SKIP_JUST_RPT = "CR_FUNDING_PLAN_SKIP_JUST_RPT";
   Other = "Other";
-  planDocDtos: Observable<DocumentsDto[]>;
+  planDocDtos: DocumentsDto[];
+  isSciRatUploaded = false;
+  isExceptionsUploaded = false;
+  isSkipUploaded = false;
+  isOtherUploaded = false;
+  sciRatDocDto: DocumentsDto = {};
+  exceptionDocDto: DocumentsDto = {};
+  skipDocDto: DocumentsDto = {};
+  otherDocDto: DocumentsDto = {};
+  activeIds: string[] = [];
+
+  @ViewChild('collapseAll') collapseAll: ElementRef<HTMLElement>;
 
   constructor(private navigationModel: NavigationStepModel,
     private planModel: PlanModel,
@@ -36,7 +48,8 @@ export class PlanStep5Component implements OnInit {
     this.documentService.getFiles(513, 'PFRP').subscribe(
       //this.documentService.getFSBudgetFiles(this.planModel.fundingPlanDto.fprId, 'PFRP').subscribe(
       result => {
-        this.planDocDtos = of(result);
+        this.planDocDtos = result;
+        this.checkUploadedDocs();
 
       }, error => {
         this.logger.error('HttpClient get request error for----- ' + error.message);
@@ -45,7 +58,91 @@ export class PlanStep5Component implements OnInit {
 
   reloadFiles(result: string): void {
     this.loadFiles();
+    let el: HTMLElement = this.collapseAll.nativeElement;
+    el.click();
   }
 
+  checkUploadedDocs(): boolean {
+    this.planDocDtos.forEach(element => {
 
+      this.resetFlags(element.docType, true, element);
+
+      // if (element.docType === DocTypeConstants.JUSTIFICATION) {
+      //   this.isSciRatUploaded = true;
+      //   this.sciRatDocDto = element;
+      // }
+
+      // if (element.docType === DocTypeConstants.OTHER) {
+      //   this.isOtherUploaded = true;
+      //   this.otherDocDto = element;
+      // }
+
+      // if (element.docType === DocTypeConstants.SKIP_JUSTIFICATION) {
+      //   this.isSkiptUploaded = true;
+      //   this.skipDocDto = element;
+      // }
+
+      // if (element.docType === DocTypeConstants.EXCEPTION_JUSTIFICATION) {
+      //   this.isExceptionsUploaded = true;
+      //   this.exceptionDocDto = element;
+      // }
+
+    });
+    return false;
+  }
+
+  resetFlags(docType: string, upload: boolean, element: DocumentsDto) {
+
+    if (docType === DocTypeConstants.JUSTIFICATION) {
+      this.isSciRatUploaded = upload;
+      this.sciRatDocDto = element;
+    }
+
+    if (docType === DocTypeConstants.OTHER) {
+      this.isOtherUploaded = upload;
+      this.otherDocDto = element;
+    }
+
+    if (docType === DocTypeConstants.SKIP_JUSTIFICATION) {
+      this.isSkipUploaded = upload;
+      this.skipDocDto = element;
+    }
+
+    if (docType === DocTypeConstants.EXCEPTION_JUSTIFICATION) {
+      this.isExceptionsUploaded = upload;
+      this.exceptionDocDto = element;
+    }
+  }
+
+  downloadFile(id: number, fileName: string): void {
+
+    this.documentService.downloadById(id)
+      .subscribe(
+        (response: HttpResponse<Blob>) => {
+          const blob = new Blob([response.body], { type: response.headers.get('content-type') });
+          saveAs(blob, fileName);
+        }
+      );
+  }
+
+  deleteDoc(id: number, docType: string) {
+    if (id !== null) {
+      this.documentService.deleteDocById(id).subscribe(
+        result => {
+          this.logger.info('Delete Success');
+          this.resetFlags(docType, false, {});
+        }
+      ), err => {
+        this.logger.error('Error while deleting the document');
+      };
+    }
+  }
+
+}
+
+export enum DocTypeConstants {
+  JUSTIFICATION = 'Justification',
+  OTHER = 'Other',
+  SKIP_JUSTIFICATION = 'SkipJustification',
+  EXCEPTION_JUSTIFICATION = 'ExceptionJustification',
 }
