c1f7b79a4551888511b3ca2dc423eae520ed436b
kanozad <doug.kanoza@gmail.com>
2021-08-16 14:59:49 -0400

    FS-240 formatting

--- src/app/funding-plan/applications-proposed-for-funding/applications-proposed-for-funding.component.ts
+++ src/app/funding-plan/applications-proposed-for-funding/applications-proposed-for-funding.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./applications-proposed-for-funding.component.css']
 })
 export class ApplicationsProposedForFundingComponent implements OnInit {
+  comments: string;
 
   constructor() { }
 
