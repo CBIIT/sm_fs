fc8f815e8ddf13a36a7ff4631ebb0d2e8389b2fa
kanozad <doug.kanoza@gmail.com>
2021-08-17 15:23:20 -0400

    FS-240 Funding plan information

--- src/app/funding-plan/funding-plan-information/funding-plan-information.component.ts
+++ src/app/funding-plan/funding-plan-information/funding-plan-information.component.ts
@@ -10,6 +10,10 @@ import { NGXLogger } from 'ngx-logger';
 })
 export class FundingPlanInformationComponent implements OnInit {
   rfaDetails: RfaPaNoticesDto[];
+  applicationsSelected: number;
+  applicationsReceived: number;
+  applicationsSkipped: number;
+  applicationsNotConsidered: number;
 
   constructor(public planModel: PlanModel,
               private rfaService: CancerActivityControllerService,
@@ -24,6 +28,30 @@ export class FundingPlanInformationComponent implements OnInit {
         this.rfaDetails.push(next);
       });
     });
+
+    this.applicationsReceived = this.planModel.allGrants.length;
+
+    this.applicationsSelected = this.planModel.allGrants.filter(g => g.selected).length;
+
+    const withinRangeGrants = this.planModel.allGrants.filter(g =>
+      (!g.notSelectableReason || g.notSelectableReason.length === 0)
+      && g.priorityScoreNum >= this.planModel.minimumScore && g.priorityScoreNum <= this.planModel.maximumScore);
+    this.logger.debug('within range:', withinRangeGrants);
+
+    this.logger.debug(withinRangeGrants.filter(g => !g.selected).length);
+
+    this.applicationsSkipped = withinRangeGrants.filter(g => !g.selected).length;
+
+    // Total number of not selectable grants
+    const totalNotSelectable = this.planModel.allGrants.filter(g => g.notSelectableReason?.length > 0).length;
+    this.logger.debug('not selectable', totalNotSelectable);
+
+    const outsideRangeGrants = this.planModel.allGrants.filter(g =>
+      (!g.notSelectableReason || g.notSelectableReason.length === 0)
+      && g.priorityScoreNum < this.planModel.minimumScore || g.priorityScoreNum > this.planModel.maximumScore);
+
+    this.applicationsNotConsidered = totalNotSelectable + outsideRangeGrants.filter(g => !g.selected).length;
+
   }
 
 
