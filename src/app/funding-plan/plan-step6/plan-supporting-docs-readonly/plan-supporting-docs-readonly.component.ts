19239e7f4c8ff24b20bce26b6e84c9ace04cdad2
Prakash Garapati <prakash.garapati@nih.gov>
2021-08-26 11:11:24 -0400

    Adding docs Readonly component for plans

---
+++ src/app/funding-plan/plan-step6/plan-supporting-docs-readonly/plan-supporting-docs-readonly.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-plan-supporting-docs-readonly',
+  templateUrl: './plan-supporting-docs-readonly.component.html',
+  styleUrls: ['./plan-supporting-docs-readonly.component.css']
+})
+export class PlanSupportingDocsReadonlyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
