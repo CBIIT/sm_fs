b4a3aefaba450870543e61f3938de2ebcc51d743
kanozad <doug.kanoza@gmail.com>
2021-08-26 13:10:52 -0400

    FS-240

--- src/app/funding-plan/fp-grant-information/fp-grant-information.component.ts
+++ src/app/funding-plan/fp-grant-information/fp-grant-information.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { NciPfrGrantQueryDtoEx } from '../../model/plan/nci-pfr-grant-query-dto-ex';
 import { PlanModel } from '../../model/plan/plan-model';
 import { GrantAwardedDto } from '@nci-cbiit/i2ecws-lib/model/grantAwardedDto';
 import { NGXLogger } from 'ngx-logger';
 import { FsRequestControllerService } from '@nci-cbiit/i2ecws-lib';
 import { PlanCoordinatorService } from '../service/plan-coordinator-service';
+import { RecommendedFutureYearsComponent } from '../recommended-future-years/recommended-future-years.component';
 
 @Component({
   selector: 'app-fp-grant-information',
@@ -12,6 +13,7 @@ import { PlanCoordinatorService } from '../service/plan-coordinator-service';
   styleUrls: ['./fp-grant-information.component.css']
 })
 export class FpGrantInformationComponent implements OnInit {
+  @ViewChild(RecommendedFutureYearsComponent) recommendedFutureYearsComponent: RecommendedFutureYearsComponent;
   @Input() grant: NciPfrGrantQueryDtoEx;
   @Input() index: number;
 
@@ -34,15 +36,24 @@ export class FpGrantInformationComponent implements OnInit {
       this.piDirect = 0;
       this.piTotal = 0;
       this.grantAwards = result;
-      result.forEach(ga => {
-        if (!isNaN(ga.requestAmount)) {
-          this.piDirect += Number(ga.requestAmount);
-        }
-        if (!isNaN(ga.requestTotalAmount)) {
-          this.piTotal += Number(ga.requestTotalAmount);
-        }
-      });
-      this.planCoordinatorService.grantInfoCostEmitter.next({index: this.index, dc: this.piDirect, tc: this.piTotal});
+      if (result && result.length > 0) {
+        this.piDirect = Number(result[0].requestAmount);
+        this.piTotal = Number(result[0].requestTotalAmount);
+
+      } else {
+        this.logger.error('No grant awards found for applid', this.grant.applId);
+        this.piDirect = 0;
+        this.piTotal = 0;
+      }
+      // result.forEach(ga => {
+      //   if (!isNaN(ga.requestAmount)) {
+      //     this.piDirect += Number(ga.requestAmount);
+      //   }
+      //   if (!isNaN(ga.requestTotalAmount)) {
+      //     this.piTotal += Number(ga.requestTotalAmount);
+      //   }
+      // });
+      this.planCoordinatorService.grantInfoCostEmitter.next({ index: this.index, dc: this.piDirect, tc: this.piTotal });
     });
   }
 
