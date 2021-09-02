bcfd8bc1584bcc0209c4daec2669d931d4542115
Prakash Garapati <prakash.garapati@nih.gov>
2021-09-02 13:34:44 -0400

    FS-33 - Changing label names

--- src/app/funding-plan/plan-step5/plan-step5.component.ts
+++ src/app/funding-plan/plan-step5/plan-step5.component.ts
@@ -35,7 +35,7 @@ export class PlanStep5Component implements OnInit {
   skipGrants: NciPfrGrantQueryDto[];
   private applIds: number[] = [];
   public btnLabel = 'Expand All';
-
+  
   @ViewChild('collapseAll') collapseAll: ElementRef<HTMLElement>;
 
   get model(): PlanModel {
@@ -151,7 +151,7 @@ export class PlanStep5Component implements OnInit {
   }
 
   openAll() {
-    
+
     if (this.btnLabel === 'Expand All') {
       this.btnLabel = 'Collapse All';
       this.activeIds = ['sciRat-panel', 'justExc-panel', 'justSkip-panel', 'other-panel'];
@@ -159,6 +159,33 @@ export class PlanStep5Component implements OnInit {
       this.btnLabel = 'Expand All';
       this.activeIds = [];
     }
+
+    this.changeLabel('sciRatNotUploadedBtnId');
+    if (this.exceptionGrants.length > 0 && !this.isExceptionsUploaded) {
+      this.changeLabel('excNotUploadedBtnId');
+    }
+    
+    if (this.skipGrants.length > 0 && !this.isSkipUploaded) {
+      this.changeLabel('skipNotUploadedBtnId');
+    }
+    
+    this.changeLabel('othNotUploadedBtnId');
+  }
+
+  changeLabel(idName: string) {
+    let body = document.getElementById(idName);
+    if (body.classList.contains("fa-plus")) {
+            let body = document.getElementById(idName);
+      body.classList.remove("fa-plus");
+      body.classList.add("fa-minus");
+      body.innerText='Hide Add Document'
+    } else {
+      
+      body.classList.remove("fa-minus");
+      body.classList.add("fa-plus");
+      body.innerText='Add Document'
+    }
+    
   }
 
   nextStep(): void {
