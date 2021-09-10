932c534ef11ff91e4cacac78ac9092bd2f63609a
esparhams <esparhams@nih.gov>
2021-09-10 10:43:25 -0400

    clean up

--- src/app/pipes/format-ni-esi-flag.pipe.ts
+++ src/app/pipes/format-ni-esi-flag.pipe.ts
@@ -7,8 +7,8 @@ export class FormatNiEsiFlagPipe implements PipeTransform {
 
   transform(value: string, ...args: unknown[]): string {
 
-    return (value ==='0') ? 'N' : (value === '1')? 'Y': (value ==='2')?'N/A':'--'        
-      
+    return (value === '0') ? 'N' : (value === '1') ? 'Y' : (value === '2') ? 'N/A' : '--';
+
   }
 
 }
