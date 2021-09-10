575975eaf1a34fc1064bdb7be6a67192148c8062
Koneru <manoja.koneru@nih.gov>
2021-09-10 10:06:30 -0400

    FS-769 Funding Request: Demo Feedback - Add "New Investigator (NI)" and "Early Stage Investigator (ESI)" information from IMPAC II and display it as read-only within the Grant Information section.

---
+++ src/app/pipes/format-ni-esi-flag.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'formatNiEsiFlag'
+})
+export class FormatNiEsiFlagPipe implements PipeTransform {
+
+  transform(value: string, ...args: unknown[]): string {
+
+    return (value ==='0') ? 'N' : (value === '1')? 'Y': (value ==='2')?'N/A':'--'        
+      
+  }
+
+}
