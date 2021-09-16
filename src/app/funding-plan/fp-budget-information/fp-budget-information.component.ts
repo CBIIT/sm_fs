7cbf329cedd316bd56419b106ffc8e1c6c8f9f68
kanozad <doug.kanoza@gmail.com>
2021-09-16 11:36:04 -0400

    FS-281

--- src/app/funding-plan/fp-budget-information/fp-budget-information.component.ts
+++ src/app/funding-plan/fp-budget-information/fp-budget-information.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PlanModel } from '../../model/plan/plan-model';
 
 @Component({
   selector: 'app-fp-budget-information',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FpBudgetInformationComponent implements OnInit {
 
-  constructor() { }
+  constructor(public planModel: PlanModel) { }
 
   ngOnInit(): void {
   }
