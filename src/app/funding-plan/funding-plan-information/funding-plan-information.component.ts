67d074f37b56adedaf50e05efe48f4b3c4366e0a
esparhams <esparhams@nih.gov>
2021-09-16 12:02:18 -0400

    Removed hardcoded items

--- src/app/funding-plan/funding-plan-information/funding-plan-information.component.ts
+++ src/app/funding-plan/funding-plan-information/funding-plan-information.component.ts
@@ -32,6 +32,7 @@ export class FundingPlanInformationComponent implements OnInit {
   listApplicationsWithinRange: NciPfrGrantQueryDtoEx[];
   otherDocs: string[];
 
+  loaMap: Map<string, string>;
 
   constructor(public planModel: PlanModel,
               private rfaService: CancerActivityControllerService,
@@ -39,6 +40,12 @@ export class FundingPlanInformationComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.loaMap = new Map<string, string>()
+      .set('PD', 'Program Director')
+      .set('DD', 'NCI Director')
+      .set('SPL', 'Scientific Program Leaders Committee')
+      .set('DAO', 'Division/Office/Center (DOC) Approver');
+      
     if(this.showTitle === undefined) {
       this.showTitle = this.showAdditionalInfo;
     }
