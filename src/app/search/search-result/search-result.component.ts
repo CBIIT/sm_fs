b9d0482590513c60ca0b97c16203aa6d08d7663b
polonskyy <yakov.polonsky@nih.gov>
2021-10-01 18:23:28 -0400

    FS-285 - Search for Funding Plans in progress

--- src/app/search/search-result/search-result.component.ts
+++ src/app/search/search-result/search-result.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, OnDestroy, OnInit, TemplateRef, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, OnDestroy, OnInit, QueryList, TemplateRef, ViewChild, ViewChildren} from '@angular/core';
 import { FundSelectSearchCriteria, FsSearchControllerService, FundingRequestQueryDatatableDto, FundingRequestQueryDto } from '@nci-cbiit/i2ecws-lib';
 import { NGXLogger } from 'ngx-logger';
 import {Subject} from "rxjs";
@@ -30,30 +30,36 @@ export class SearchResultComponent implements OnInit, AfterViewInit, OnDestroy {
               private loaderService: LoaderService,
               private logger: NGXLogger) { }
 
-  @ViewChild(DataTableDirective, {static: false}) dtElement: DataTableDirective;
+  @ViewChildren(DataTableDirective) dtElements: QueryList<DataTableDirective>;
+
   @ViewChild('selectFundingRequestCheckboxRenderer') selectFundingRequestCheckboxRenderer: TemplateRef<SelectFundingRequestCheckboxCellRendererComponent>;
   @ViewChild('fullGrantNumberRenderer') fullGrantNumberRenderer: TemplateRef<FullGrantNumberCellRendererComponent>;
   @ViewChild('cancerActivityRenderer') cancerActivityRenderer: TemplateRef<CancerActivityCellRendererComponent>;
   @ViewChild('searchFundingRequestActionRenderer') searchFundingRequestActionRenderer: TemplateRef<SearchFundingRequestActionCellRendererComponent>;
 
+  @ViewChild('selectFundingPlanCheckboxRenderer') selectFundingPlanCheckboxRenderer: TemplateRef<SelectFundingRequestCheckboxCellRendererComponent>;
+  @ViewChild('searchFundingPlanActionRenderer') searchFundingPlanActionRenderer: TemplateRef<SearchFundingRequestActionCellRendererComponent>;
+
   // dtOptions: DataTables.Settings = {};
-  dtOptions: any = {};
-  dtTrigger: Subject<any> = new Subject();
+
   grantViewerUrl: string = this.propertiesService.getProperty('GRANT_VIEWER_URL');
   eGrantsUrl: string = this.propertiesService.getProperty('EGRANTS_URL');
 
   dtFundingRequestOptions: any = {};
   dtFundingPlanOptions: any = {};
+  dtFundingRequestTrigger: Subject<any> = new Subject();
+  dtFundingPlanTrigger: Subject<any> = new Subject();
 
   fundingRequests: FundingRequestQueryDto[];
   fundingPlans: FundingPlanQueryDto[];
   searchCriteria: FundSelectSearchCriteria;
-  params: any;
 
-  searchResult;
-  noResult: boolean;
+  noFundingRequestResult: boolean;
+  noFundingPlanResult: boolean;
   filterTypeLabel: string;
 
+  ngOnInit(): void {
+  }
 
   ngAfterViewInit(): void {
     this.dtFundingRequestOptions = {
@@ -61,6 +67,7 @@ export class SearchResultComponent implements OnInit, AfterViewInit, OnDestroy {
       pageLength: 100,
       serverSide: true,
       processing: false,
+      destroy: true,
       language: {
         paginate: {
           first: '<i class="far fa-chevron-double-left" title="First"></i>',
@@ -71,7 +78,7 @@ export class SearchResultComponent implements OnInit, AfterViewInit, OnDestroy {
       },
       ajax: (dataTablesParameters: any, callback) => {
         if (!this.searchCriteria) {
-          this.noResult = true;
+          this.noFundingRequestResult = true;
           callback({
             recordsTotal: 0,
             recordsFiltered: 0,
@@ -89,7 +96,7 @@ export class SearchResultComponent implements OnInit, AfterViewInit, OnDestroy {
             this._populateSelectedIntoResults(result.data);
             // this.logger.debug('Search Funding Requests result: ', result);
             this.fundingRequests = result.data;
-            this.noResult = result.recordsTotal <= 0;
+            this.noFundingRequestResult = result.recordsTotal <= 0;
             callback({
               recordsTotal: result.recordsTotal,
               recordsFiltered: result.recordsFiltered,
@@ -98,7 +105,7 @@ export class SearchResultComponent implements OnInit, AfterViewInit, OnDestroy {
             this.loaderService.hide();
           }, error => {
             this.logger.error('HttpClient get request error for----- ' + error.message);
-            this.noResult = true;
+            this.noFundingRequestResult = true;
             callback({
               recordsTotal: 0,
               recordsFiltered: 0,
@@ -183,7 +190,7 @@ export class SearchResultComponent implements OnInit, AfterViewInit, OnDestroy {
         // Fix for Excel output - I removed empty renderers in column definitions
         // But now, I have to remove the first "text" child node to prevent it
         // from rendering (angular datatables bug)
-        this.dtOptions.columns.forEach((column, ind) => {
+        this.dtFundingRequestOptions.columns.forEach((column, ind) => {
           if (column.ngTemplateRef) {
             const cell = row.childNodes.item(ind);
             if (cell.childNodes.length > 1) { // you have to leave at least one child node
@@ -199,6 +206,7 @@ export class SearchResultComponent implements OnInit, AfterViewInit, OnDestroy {
       pageLength: 100,
       serverSide: true,
       processing: false,
+      destroy: true,
       language: {
         paginate: {
           first: '<i class="far fa-chevron-double-left" title="First"></i>',
@@ -209,7 +217,7 @@ export class SearchResultComponent implements OnInit, AfterViewInit, OnDestroy {
       },
       ajax: (dataTablesParameters: any, callback) => {
         if (!this.searchCriteria) {
-          this.noResult = true;
+          this.noFundingPlanResult = true;
           callback({
             recordsTotal: 0,
             recordsFiltered: 0,
@@ -227,7 +235,7 @@ export class SearchResultComponent implements OnInit, AfterViewInit, OnDestroy {
             this._populateSelectedIntoResults(result.data);
             // this.logger.debug('Search Funding Requests result: ', result);
             this.fundingPlans = result.data;
-            this.noResult = result.recordsTotal <= 0;
+            this.noFundingPlanResult = result.recordsTotal <= 0;
             callback({
               recordsTotal: result.recordsTotal,
               recordsFiltered: result.recordsFiltered,
@@ -240,35 +248,29 @@ export class SearchResultComponent implements OnInit, AfterViewInit, OnDestroy {
       },
       columns: [
         {title: 'Sel', data: 'selected', orderable: false, ngTemplateRef: { ref: this.selectFundingRequestCheckboxRenderer }, className: 'all' }, // 0
-        {title: 'Grant Number', data: 'fullGrantNum', ngTemplateRef: { ref: this.fullGrantNumberRenderer}, className: 'all'}, // 1
-        {title: 'PI', data: 'piFullName', render: ( data, type, row, meta ) => {
-            return (!data || data == null) ? '' : '<a href="mailto:' + row.piEmail + '?subject=' + row.fullGrantNum + ' - ' + row.lastName + '">' + data + '</a>';
-          }, className: 'all'}, // 2
-        {title: 'Project Title', data: 'projectTitle'}, // 3
-        {title: 'I2 Status', data: 'applStatusGroupDescrip'}, // 4
-        {title: 'PD', data: 'pdFullName', render: ( data, type, row, meta ) => {
-            return (!data || data == null) ? '' : '<a href="mailto:' + row.pdEmailAddress + '?subject=' + row.fullGrantNum + ' - ' + row.lastName + '">' + data + '</a>';
+        {title: 'RFA/PA info - Number / Title / NCAB / Issue Type', data: 'comments', className: 'all'}, // 1
+        {title: 'Plan ID', data: 'fprId'}, // 2
+        {title: 'Plan Name', data: 'planName'}, // 3
+        {title: 'Requesting PD & DOC', data: 'requestorPdFullName', render: ( data, type, row, meta ) => {
+            return (!data || data == null) ? '' : '<a href="mailto:' + row.requestorEmailAddress + '?subject=' + row.planName + ' - ' + row.requestorPdFullName + '">' + data + '</a>';
+          }}, // 4
+        {title: 'Requesting DOC Approver', data: 'comments', render: ( data, type, row, meta ) => { //TODO
+            return (!data || data == null) ? '' : '<a href="mailto:' + row.requestorEmailAddress + '?subject=' + row.planName + ' - ' + row.requestorPdFullName + '">' + data + '</a>';
           }}, // 5
-        {title: 'CA', data: 'cayCode', ngTemplateRef: { ref: this.cancerActivityRenderer}, className: 'all'}, // 6
-        {title: 'FY', data: 'fy'}, // 7
-        {title: 'Request ID', data: 'frqId'}, // 8
-        {title: 'Request Name', data: 'requestName'}, // 9
-        {title: 'Request Type', data: 'requestType'}, // 10
-        {title: 'Requesting DOC Approver', data: 'approverFullName', render: ( data, type, row, meta ) => {
-            return (!data || data == null) ? '' : '<a href="mailto:' + row.approverEmailAddress + '?subject=' + row.fullGrantNum + ' - ' + row.lastName + '">' + data + '</a>';
-          }}, // 11
-        {title: 'Final LOA', data: 'loaName'}, // 12
-        {title: 'Funding Approvals', data: 'fundsCertificationCode'}, // 13
-        {title: 'Status', data: 'currentStatusDescrip'}, // 14
-        {title: 'Last Action Date', data: 'requestStatusDate'}, // 15
+        {title: 'Final LOA', data: 'loaName'}, // 6
+        {title: 'Funding Approvals', data: 'comments'}, // 7  //TODO
+        {title: 'Program Recomm. Direct Costs', data: 'comments'}, // 8  //TODO
+        {title: 'Program Recomm. Total Costs', data: 'comments'}, // 9  //TODO
+        {title: 'Status', data: 'planStatus'}, // 10
+        {title: 'Last Action Date', data: 'planStatusDate'}, // 11
         {
           title: 'Action', data: null, defaultContent: 'Select'
           ,ngTemplateRef: { ref: this.searchFundingRequestActionRenderer}, className: 'all'
-        }, // 16
+        }, // 12
         {data: null, defaultContent: ''}
 
       ],
-      order: [[8, 'asc']],
+      // order: [[8, 'asc']],
       responsive: {
         details: {
           type: 'column',
@@ -281,21 +283,18 @@ export class SearchResultComponent implements OnInit, AfterViewInit, OnDestroy {
           orderable: false,
           targets: -1
         },
-        {responsivePriority: 1, targets: 1 }, // grant_num
-        {responsivePriority: 2, targets: 16 }, // action
-        {responsivePriority: 3, targets: 2 }, // pi
-        {responsivePriority: 5, targets: 7 }, // fy
-        {responsivePriority: 6, targets: 5 }, // pd
-        {responsivePriority: 7, targets: 6 }, // ca
-        {responsivePriority: 8, targets: 14 }, // status
-        {responsivePriority: 9, targets: 10 }, // request type
-        {responsivePriority: 10, targets: 8 }, // reuest id
-        {responsivePriority: 11, targets: 9 }, // request name
-        {responsivePriority: 12, targets: 12 }, // final loa
-        {responsivePriority: 13, targets: 11 }, // requesting doc approver
-        {responsivePriority: 13, targets: 13 }, // funding approvals
-        {responsivePriority: 13, targets: 4 }, // i2 status
-        {responsivePriority: 14, targets: 3 } // project title
+        {responsivePriority: 1, targets: 1 }, // rfa/pa info
+        // {responsivePriority: 2, targets: 12 }, // action
+        {responsivePriority: 3, targets: 2 }, // plan id
+        {responsivePriority: 5, targets: 4 }, // requesting pd & doc
+        {responsivePriority: 6, targets: 5 }, // requesting doc approver
+        {responsivePriority: 7, targets: 10 }, // status
+        {responsivePriority: 8, targets: 11 }, // last action date
+        {responsivePriority: 9, targets: 3 }, // plan name
+        {responsivePriority: 10, targets: 6 }, // final LOA
+        {responsivePriority: 11, targets: 7 }, // funding approvals
+        {responsivePriority: 12, targets: 9 }, // program recomm. Total Costs
+        {responsivePriority: 13, targets: 8 } // program recomm. Direct Costs
       ],
       dom: '<"dt-controls"l<"ml-auto"fB<"d-inline-block"p>>>rt<"dt-controls"<"mr-auto"i>p>',
       buttons: [
@@ -307,14 +306,14 @@ export class SearchResultComponent implements OnInit, AfterViewInit, OnDestroy {
           filename: 'fs-funding-requests-search-result',
           title: null,
           header: true,
-          exportOptions: { columns: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13] }
+          exportOptions: { columns: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11] } //TODO
         }
       ],
       rowCallback: (row: Node, data: any[] | object, index: number) => {
         // Fix for Excel output - I removed empty renderers in column definitions
         // But now, I have to remove the first "text" child node to prevent it
         // from rendering (angular datatables bug)
-        this.dtOptions.columns.forEach((column, ind) => {
+        this.dtFundingPlanOptions.columns.forEach((column, ind) => {
           if (column.ngTemplateRef) {
             const cell = row.childNodes.item(ind);
             if (cell.childNodes.length > 1) { // you have to leave at least one child node
@@ -325,46 +324,55 @@ export class SearchResultComponent implements OnInit, AfterViewInit, OnDestroy {
       }
     };
 
-    this.dtOptions = this.dtFundingRequestOptions;
-
     setTimeout(() => {
-      // // this.search();
-      this.dtTrigger.next();
+      this.dtFundingRequestTrigger.next();
+      this.dtFundingPlanTrigger.next();
     }, 0);
   }
 
-    ngOnInit(): void {
-  }
 
   ngOnDestroy(): void {
-    this.dtTrigger.unsubscribe();
+    this.dtFundingRequestTrigger.unsubscribe();
+    this.dtFundingPlanTrigger.unsubscribe();
   }
 
   doFundingRequestSearch(criteria: FundSelectSearchCriteria, filterTypeLabel: string): void {
     this.searchCriteria = criteria;
-    this.noResult = true;
+    this.noFundingPlanResult = true;
+    this.noFundingRequestResult = false;
     this.filterTypeLabel = filterTypeLabel;
-    if (this.dtElement.dtInstance) {
-      this.dtElement.dtInstance.then((dtInstance: DataTables.Api) => {
-        dtInstance.destroy();
-        this.dtOptions = this.dtFundingRequestOptions;
-        setTimeout(() => this.dtTrigger.next(), 0);
-        // this.dtTrigger.next();
-      });
-    }
+    this.dtElements.forEach((dtEl: DataTableDirective) => {
+      console.log('dtElement', dtEl);
+      if (dtEl.dtInstance) {
+        dtEl.dtInstance.then((dtInstance: DataTables.Api) => {
+          dtInstance.clear();
+          if (dtEl.dtTrigger === this.dtFundingRequestTrigger) {
+            console.log('FR dtInstance', dtInstance);
+            setTimeout(() => this.dtFundingRequestTrigger.next(), 0);
+          }
+        });
+      }
+    });
   }
 
   doFundingPlanSearch(criteria: FundSelectSearchCriteria, filterTypeLabel: string): void {
     this.searchCriteria = criteria;
-    this.noResult = true;
+    this.noFundingRequestResult = true;
+    this.noFundingPlanResult = false;
     this.filterTypeLabel = filterTypeLabel;
-    if (this.dtElement.dtInstance) {
-      this.dtElement.dtInstance.then((dtInstance: DataTables.Api) => {
-        dtInstance.destroy();
-        this.dtOptions = this.dtFundingPlanOptions;
-        setTimeout(() => this.dtTrigger.next(), 0);
-      });
-    }
+
+    this.dtElements.forEach((dtEl: DataTableDirective) => {
+      console.log('dtElement', dtEl);
+      if (dtEl.dtInstance) {
+        dtEl.dtInstance.then((dtInstance: DataTables.Api) => {
+          dtInstance.clear();
+          if (dtEl.dtTrigger === this.dtFundingPlanTrigger) {
+            console.log('FP dtInstance', dtInstance);
+            setTimeout(() => this.dtFundingPlanTrigger.next(), 0);
+          }
+        });
+      }
+    });
   }
 
   onCaptureSelectedEvent($event: any) {
