246b4d4c0663be481c017994813d9c5ed0e68d56
kanozad <doug.kanoza@gmail.com>
2021-11-09 07:01:16 -0500

    Timeout interceptor

--- src/app/interceptors/timeout.interceptor.ts
+++ src/app/interceptors/timeout.interceptor.ts
@@ -1,19 +1,24 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { NGXLogger } from "ngx-logger";
-import { Observable } from "rxjs";
-import { TimeoutService } from "../service/timeout.service";
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { NGXLogger } from 'ngx-logger';
+import { Observable } from 'rxjs';
+import { TimeoutService } from '../service/timeout.service';
+import { catchError } from 'rxjs/operators';
 
 @Injectable()
-export class TimeoutInterceptor  implements HttpInterceptor {
-    constructor(
-        private timeoutService: TimeoutService,
-        private logger: NGXLogger) {
-        
-    }
+export class TimeoutInterceptor implements HttpInterceptor {
+  constructor(
+    private timeoutService: TimeoutService,
+    private logger: NGXLogger) {
 
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        return next.handle(req);
-    }
-    
+  }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(
+      catchError((error, caught) => {
+        this.logger.error('-- error --', error, caught);
+        return caught;
+      })
+    );
+  }
 }
