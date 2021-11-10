c0af45a7bae71b488c9d652d29a43f66a20be687
polonskyy <yakov.polonsky@nih.gov>
2021-11-10 10:09:40 -0500

    FS-1112 - add space after fy range

--- src/environments/proxy.config.js
+++ src/environments/proxy.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   '/i2ecws': {
-    target: 'http://localhost:8080/',
+    target: 'http://localhost:51080/',
+    secure: false,
+    changeOrigin: true,
+    onProxyRes: proxyRes => {
+      let key = 'www-authenticate';
+      proxyRes.headers[key] = proxyRes.headers[key] && proxyRes.headers[key].split(',');
+    }
+  },
+  '/paylist': {
+    target: 'http://localhost:80/',
     secure: false,
     changeOrigin: true,
     onProxyRes: proxyRes => {
