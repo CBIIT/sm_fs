82a7659bf6e84fc8af2208ef6bdcd8da92e5bc1d
polonskyy <yakov.polonsky@nih.gov>
2021-11-10 10:24:02 -0500

    revert back proxy.config and package.json

--- src/environments/proxy.config.js
+++ src/environments/proxy.config.js
@@ -1,15 +1,6 @@
 module.exports = {
   '/i2ecws': {
-    target: 'http://localhost:51080/',
-    secure: false,
-    changeOrigin: true,
-    onProxyRes: proxyRes => {
-      let key = 'www-authenticate';
-      proxyRes.headers[key] = proxyRes.headers[key] && proxyRes.headers[key].split(',');
-    }
-  },
-  '/paylist': {
-    target: 'http://localhost:80/',
+    target: 'http://localhost:8080/',
     secure: false,
     changeOrigin: true,
     onProxyRes: proxyRes => {
