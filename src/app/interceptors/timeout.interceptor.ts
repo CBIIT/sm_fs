9e61f9c5163a94256dfdb2c604fa52e52d1e25e1
kanozad <doug.kanoza@gmail.com>
2021-11-22 10:09:52 -0500

    FS-666

--- src/app/interceptors/timeout.interceptor.ts
+++ src/app/interceptors/timeout.interceptor.ts
@@ -6,13 +6,17 @@ import { catchError } from 'rxjs/operators';
 import { Router } from '@angular/router';
 import { openNewWindow } from '../utils/utils';
 import { ErrorHandlerService } from '../error/error-handler.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { FundingSourceEntryModalComponent } from '../funding-plan/applications-proposed-for-funding/funding-source-entry-modal/funding-source-entry-modal.component';
+import { SessionRestoreComponent } from '../session/session-restore/session-restore.component';
 
 @Injectable()
 export class TimeoutInterceptor implements HttpInterceptor {
   constructor(
     private errorHandler: ErrorHandlerService,
     private logger: NGXLogger,
-    private router: Router
+    private router: Router,
+    private modalService: NgbModal
   ) {
   }
 
@@ -22,18 +26,19 @@ export class TimeoutInterceptor implements HttpInterceptor {
       catchError((error, caught) => {
         // this.logger.warn(`Raw error: ${JSON.stringify(error)}`);
 
-        const timestamp: number = Date.now();
-        this.errorHandler.registerNewError(timestamp, error);
-
         if (error.status === 200 && error.url?.startsWith('https://auth')) {
           this.logger.warn('Error is most likely timeout - redirect to login.');
           const url = '/fs/#' + this.router.createUrlTree(['restoreSession']).toString();
-          openNewWindow(url, 'Restore Session', undefined);
-          // return null;
+          const modalRef = this.modalService.open(SessionRestoreComponent, { size: 'lg' });
+          modalRef.result.then(() => {
+            return next.handle(req);
+          });
         } else {
+          const timestamp: number = Date.now();
+          this.errorHandler.registerNewError(timestamp, error);
+          this.router.navigate(['/error', timestamp]);
+          throw error;
         }
-        this.router.navigate(['/error', timestamp]);
-        throw error;
       })
     );
   }
