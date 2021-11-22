2a4c1e80f944088ba54b3e6d0da1b71ffa5b1065
kanozad <doug.kanoza@gmail.com>
2021-11-22 14:48:57 -0500

    FS-666

--- src/app/interceptors/timeout.interceptor.ts
+++ src/app/interceptors/timeout.interceptor.ts
@@ -1,13 +1,11 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { NGXLogger } from 'ngx-logger';
-import { from, Observable } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { from, Observable, of, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 import { Router } from '@angular/router';
-import { openNewWindow } from '../utils/utils';
 import { ErrorHandlerService } from '../error/error-handler.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { FundingSourceEntryModalComponent } from '../funding-plan/applications-proposed-for-funding/funding-source-entry-modal/funding-source-entry-modal.component';
 import { SessionRestoreComponent } from '../session/session-restore/session-restore.component';
 
 @Injectable()
@@ -22,24 +20,26 @@ export class TimeoutInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     // this.logger.debug(req, next);
+
     return next.handle(req).pipe(
+      retry(1),
       catchError((error, caught) => {
         // this.logger.warn(`Raw error: ${JSON.stringify(error)}`);
 
         if (error.status === 200 && error.url?.startsWith('https://auth')) {
           this.logger.warn('Error is most likely timeout - redirect to login.');
-          const url = '/fs/#' + this.router.createUrlTree(['restoreSession']).toString();
+          // const url = '/fs/#' + this.router.createUrlTree(['restoreSession']).toString();
           const modalRef = this.modalService.open(SessionRestoreComponent, { size: 'lg' });
           const obs = from(modalRef.result.then(() => {
             return next.handle(req);
           }));
-          this.logger.debug('return observable thing: ', obs);
-          return obs as unknown as Observable<HttpEvent<any>>;
+          return throwError(error);
+          // return obs as unknown as Observable<HttpEvent<any>>;
         } else {
           const timestamp: number = Date.now();
           this.errorHandler.registerNewError(timestamp, error);
           this.router.navigate(['/error', timestamp]);
-          throw error;
+          return throwError(error);
         }
       })
     );
