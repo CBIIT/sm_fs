95272cab0531e8b30cac7057592203d36da79ecc
kanozad <doug.kanoza@gmail.com>
2021-12-02 08:10:31 -0500

    FS-1275

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -67,7 +67,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  imports: [RouterModule.forRoot(routes, { useHash: true, scrollPositionRestoration: 'enabled' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
