c0023468b146a4caf87cd5a403ea835a838e2319
kanozad <doug.kanoza@gmail.com>
2021-12-03 08:41:53 -0500

    FS-666 Interim

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -37,6 +37,7 @@ const routes: Routes = [
       { path: 'search/grants', component: SearchComponent, canActivate: [AuthGuard] },
       { path: 'search/:action', component: SearchComponent, canActivate: [AuthGuard] },
       { path: 'search', component: SearchComponent, canActivate: [AuthGuard] },
+      { path: 'restoreSession', component: SessionRestoreComponent },
       { path: 'plan', component: FundingPlanComponent, children: [
         {path: '', redirectTo: 'step1/new', pathMatch: 'full'},
         {path: 'step1', redirectTo: 'step1/', pathMatch: 'full'},
@@ -62,7 +63,6 @@ const routes: Routes = [
     ]
   },
   {path: 'error/:errorId', component: ErrorComponent},
-  {path: 'restoreSession', component: SessionRestoreComponent},
   { path: 'unauthorize', component: UnauthorizeComponent }
 ];
 
