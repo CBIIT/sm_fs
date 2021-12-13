25876e08bd5d0cfae68e02228cfab7eb156a4c63
kanozad <doug.kanoza@gmail.com>
2021-12-13 14:01:22 -0500

    FS-1327

--- src/app/funding-plan/fp-funding-information/fp-funding-information.component.ts
+++ src/app/funding-plan/fp-funding-information/fp-funding-information.component.ts
@@ -19,6 +19,108 @@ export class FpFundingInformationComponent implements OnInit {
   outYear4: number = null;
   outYear5: number = null;
 
+  get firstYearSetAsideDisplay(): string {
+    if (this.firstYearSetAside) {
+      return this.firstYearSetAside.toLocaleString();
+    }
+    return null;
+  }
+
+  set firstYearSetAsideDisplay(value: string) {
+    this.firstYearSetAside = null;
+    if (value) {
+      value = value.replace(/\,/g, '');
+      if (value) {
+        this.firstYearSetAside = Number(value);
+      }
+    }
+  }
+
+  get totalSetAsideDisplay(): string {
+    if (this.totalSetAside) {
+      return this.totalSetAside.toLocaleString();
+    }
+    return null;
+  }
+
+  set totalSetAsideDisplay(value: string) {
+    this.totalSetAside = null;
+    if (value) {
+      value = value.replace(/\,/g, '');
+      if (value) {
+        this.totalSetAside = Number(value);
+      }
+    }
+  }
+
+  get outYear2Display(): string {
+    if (this.outYear2) {
+      return this.outYear2.toLocaleString();
+    }
+    return null;
+  }
+
+  set outYear2Display(value: string) {
+    this.outYear2 = null;
+    if (value) {
+      value = value.replace(/\,/g, '');
+      if (value) {
+        this.outYear2 = Number(value);
+      }
+    }
+  }
+
+  get outYear3Display(): string {
+    if (this.outYear3) {
+      return this.outYear3.toLocaleString();
+    }
+    return null;
+  }
+
+  set outYear3Display(value: string) {
+    this.outYear3 = null;
+    if (value) {
+      value = value.replace(/\,/g, '');
+      if (value) {
+        this.outYear3 = Number(value);
+      }
+    }
+  }
+
+  get outYear4Display(): string {
+    if (this.outYear4) {
+      return this.outYear4.toLocaleString();
+    }
+    return null;
+  }
+
+  set outYear4Display(value: string) {
+    this.outYear4 = null;
+    if (value) {
+      value = value.replace(/\,/g, '');
+      if (value) {
+        this.outYear4 = Number(value);
+      }
+    }
+  }
+
+  get outYear5Display(): string {
+    if (this.outYear5) {
+      return this.outYear5.toLocaleString();
+    }
+    return null;
+  }
+
+  set outYear5Display(value: string) {
+    this.outYear5 = null;
+    if (value) {
+      value = value.replace(/\,/g, '');
+      if (value) {
+        this.outYear5 = Number(value);
+      }
+    }
+  }
+
   constructor(
     private planModel: PlanModel) {
   }
@@ -31,6 +133,7 @@ export class FpFundingInformationComponent implements OnInit {
     this.outYear4 = this.planModel.fundingPlanDto.fundingEstYr4Amt;
     this.outYear5 = this.planModel.fundingPlanDto.fundingEstYr5Amt;
   }
+
 
   get firstYearTotalCalculated(): number {
     return this.planModel.fundingPlanDto.totalRecommendedAmt;
--- src/app/validators/min-max-validator-directive.ts
+++ src/app/validators/min-max-validator-directive.ts
@@ -1,10 +1,10 @@
-import {AbstractControl, NG_VALIDATORS, ValidationErrors, Validator} from '@angular/forms';
-import {Directive, Input} from '@angular/core';
-import {NGXLogger} from 'ngx-logger';
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
+import { Directive, Input } from '@angular/core';
+import { NGXLogger } from 'ngx-logger';
 
 @Directive({
   selector: '[appMinMaxValidator]',
-  providers: [{provide: NG_VALIDATORS, useExisting: MinMaxValidatorDirective, multi: true}]
+  providers: [{ provide: NG_VALIDATORS, useExisting: MinMaxValidatorDirective, multi: true }]
 })
 export class MinMaxValidatorDirective implements Validator {
   @Input('appMinMaxValidator') minMax: { min: number, max: number };
@@ -15,15 +15,18 @@ export class MinMaxValidatorDirective implements Validator {
 
   validate(control: AbstractControl): ValidationErrors | null {
 
-    const val = control.value;
+    let val = control.value;
+    if (val) {
+      val = val.replace(/\,/g, '');
+    }
     if (isNaN(val)) {
       return null;
     }
     if (Number(val) < this.minMax.min) {
-      return {min: true};
+      return { min: true };
     }
     if (Number(val) > this.minMax.max) {
-      return {max: true};
+      return { max: true };
     }
     return null;
   }
