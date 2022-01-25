05840f84aa01994935ed6b78a2f1b417b4e6e14c
kanozad <doug.kanoza@gmail.com>
2022-01-25 14:14:40 -0500

    FS-666 Navigation experiment

--- src/app/interceptors/error-interceptor.service.ts
+++ src/app/interceptors/error-interceptor.service.ts
@@ -60,10 +60,11 @@ export class ErrorInterceptorService implements HttpInterceptor {
           errorUrl.searchParams.delete('TARGET');
           errorUrl.searchParams.set('TARGET', url);
 
-          if (!this.modalWindow) {
-            this.modalWindow = openNewWindow(errorUrl.toString(), 'Restore_Session', features);
-          }
-          // this.router.navigate(['']);
+          // if (!this.modalWindow) {
+          //   this.modalWindow = openNewWindow(errorUrl.toString(), 'Restore_Session', features);
+          // }
+
+          this.router.navigate(['/search/fr/landing']);
           return of(undefined);
         } else {
           const timestamp: number = Date.now();
