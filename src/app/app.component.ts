a2d56c14371d0d1fc24e66e33887833899ae0c92
Bin Li <bin.li@itgfirm.com>
2022-03-02 09:48:50 -0500

    FS-1548

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import {Component, Inject} from '@angular/core';
+import {Component, Inject, isDevMode} from '@angular/core';
 import {Router} from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,8 @@ import {Router} from '@angular/router';
 export class AppComponent {
 
   constructor(public router: Router) {
+    console.log('production flag in environment?: ' + environment.production);
+    console.log('is DevMode?: ' + isDevMode());
   }
 
   title = 'Funding Selections';
