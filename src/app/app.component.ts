481037ee8322b1ae05d629aaed4412fce085ccbb
Bin Li <bin.li@itgfirm.com>
2022-04-07 15:58:54 -0400

    Fix property reading timing issue in plan and request models.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, isDevMode } from '@angular/core';
 import { Router } from '@angular/router';
 import { NGXLogger } from 'ngx-logger';
 import { environment } from 'src/environments/environment';
+import { PlanModel } from './model/plan/plan-model';
+import { RequestModel } from './model/request/request-model';
 
 @Component({
   selector: 'app-root',
@@ -11,9 +13,13 @@ import { environment } from 'src/environments/environment';
 export class AppComponent {
 
   constructor(public router: Router,
-              private logger: NGXLogger) {
+              private logger: NGXLogger,
+              private requestModel: RequestModel,
+              private planModel: PlanModel) {
     this.logger.debug('production flag in environment?: ' + environment.production);
     this.logger.debug('is DevMode?: ' + isDevMode());
+    this.requestModel.initializeProperties();
+    this.planModel.initializeProperties();
   }
 
   title = 'Funding Selections';
