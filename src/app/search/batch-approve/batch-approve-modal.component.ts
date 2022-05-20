7d7712bce123164623b8f1c736fca7d4bb3736aa
kanozad <doug.kanoza@gmail.com>
2022-05-20 07:36:42 -0400

    FS-1595

--- src/app/search/batch-approve/batch-approve-modal.component.ts
+++ src/app/search/batch-approve/batch-approve-modal.component.ts
@@ -44,7 +44,7 @@ export class BatchApproveModalComponent implements OnInit {
   maxDate: NgbDate = this.calendar.getToday();
 
   requestOrPlan: 'REQUEST'|'PLAN';
-  mode: 'DOC'|'OTHERDOC'|'SPL';
+  mode: 'DOC'|'SPL';
   buttonText = '';
   title = '';
   alert: Alert;
@@ -73,7 +73,7 @@ export class BatchApproveModalComponent implements OnInit {
     this.batchApproveSuccess = false;
     this.requestsForApproval = requests.filter(r => this.batchApproveService.canApproveRequest(r.frqId));
     this.requestOrPlan = 'REQUEST';
-    this.mode = this.batchApproveService.isDoc ? 'DOC' || 'OTHERDOC' : 'SPL';
+    this.mode = this.batchApproveService.isSpl() ? 'SPL' : 'DOC';
     this.title = 'Request(s) Selected for Batch Approval';
     this.buttonText = 'Confirm Approve';
     this.eligibleCount = this.requestsForApproval.length;
