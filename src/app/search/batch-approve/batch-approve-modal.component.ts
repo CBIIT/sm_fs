4203f6f1b270fe49236d3e8f5ce909cd8134d892
Bin Li <bin.li@itgfirm.com>
2023-04-14 15:04:48 -0400

    FS-1278

--- src/app/search/batch-approve/batch-approve-modal.component.ts
+++ src/app/search/batch-approve/batch-approve-modal.component.ts
@@ -161,5 +161,12 @@ export class BatchApproveModalComponent implements OnInit {
   convert(ncabs: string ): string {
     return convertNcabs(ncabs);
   }
+
+  public getNcabDate(ncabDate: string): string {
+    if (ncabDate && ncabDate.substring(4, 6) === '00') {
+       return '';
+    }
+    return ncabDate.substring(4, 6) + '/' + ncabDate.substring(0, 4);
+  }
 }
 
