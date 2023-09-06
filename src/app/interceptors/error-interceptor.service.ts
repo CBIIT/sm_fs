a0a99fe2425e317038ae848e2c1fcb82bdfaa621
kanozad <doug.kanoza@gmail.com>
2023-09-06 07:55:41 -0400

    FS-1768 Additional refactoring of ErrorInterceptor

--- src/app/interceptors/error-interceptor.service.ts
+++ src/app/interceptors/error-interceptor.service.ts
@@ -1,18 +1,20 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
-import { ApplicationInitStatus, Inject, Injectable, InjectionToken } from "@angular/core";
-import { EMPTY, Observable, of, throwError } from "rxjs";
+import { ApplicationInitStatus, Inject, Injectable, InjectionToken } from '@angular/core';
+import { EMPTY, Observable, of, throwError } from 'rxjs';
 import { catchError, filter, finalize } from 'rxjs/operators';
 import { NavigationEnd, NavigationStart, Router } from '@angular/router';
 import { ErrorHandlerService } from '../error/error-handler.service';
-import { openNewWindow, jsonStringifyRecursive } from '../utils/utils';
+import { jsonStringifyRecursive, openNewWindow } from '../utils/utils';
 import { Location } from '@angular/common';
 import { CustomServerLoggingService, HeartbeatService } from '@cbiit/i2ecui-lib';
 
-export const DEBUG_ERROR_INTERCEPTOR = new InjectionToken<boolean>('debugMode', {providedIn: 'root', factory: () => false});
+export const DEBUG_ERROR_INTERCEPTOR = new InjectionToken<boolean>('debugMode', {
+  providedIn: 'root',
+  factory: () => false,
+});
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-
   private modalWindow: any;
   private handled401 = false;
 
@@ -25,11 +27,11 @@ export class ErrorInterceptor implements HttpInterceptor {
     private heartbeatService: HeartbeatService,
     @Inject(DEBUG_ERROR_INTERCEPTOR) private debugMode: boolean
   ) {
-    router.events.pipe(filter(event => event instanceof NavigationStart)).subscribe((event: NavigationStart) => {
+    router.events.pipe(filter((event) => event instanceof NavigationStart)).subscribe((event: NavigationStart) => {
       this.handleNavigationStart(event);
     });
 
-    router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe((event: NavigationEnd) => {
+    router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe((event: NavigationEnd) => {
       this.handleNavigationEnd(event);
     });
   }
@@ -42,81 +44,106 @@ export class ErrorInterceptor implements HttpInterceptor {
     }
     return next.handle(req).pipe(
       catchError((error) => {
-        if(this.debugMode) this.verboseDetails(req, error)
+        if (this.debugMode) this.verboseDetails(req, error);
 
         // Pass along errors that happen during initialization
         if (!this.initializerStatus.done) {
-          if(this.debugMode) this.logger.debug('Initialization in progress. Let the error pass.');
+          if (this.debugMode) this.logger.debug('Initialization in progress. Let the error pass.');
           return throwError(error);
         }
 
         // Throw 400 errors automatically
-        if (error.status === 400) {  // BadRequestException, checked exception from backend.
-          if(this.debugMode) this.logger.debug('400: let it pass');
+        if (error.status === 400) {
+          // BadRequestException, checked exception from backend.
+          if (this.debugMode) this.logger.debug('400: let it pass');
           return throwError(error);
         }
 
         // Let the logger handle its own problems
         if (error.url.includes('logs') || req.url.includes('logs')) {
-          if(this.debugMode) this.logger.debug('Let the logger handle its own messes');
+          if (this.debugMode) this.logger.debug('Let the logger handle its own messes');
           return throwError(error);
         }
 
         // Let the heartbeat & db-heartbeat services handle everything but timeouts
         if ((error.url.includes('heartbeat') || req.url.includes('heartbeat')) && !(error.status === 200)) {
-          if(this.debugMode) this.logger.debug('Non-200 error from heartbeat');
+          if (this.debugMode) this.logger.debug('Non-200 error from heartbeat');
           return throwError(error);
         }
 
-        if (error.status === 401) {
-          this.heartbeatService.pause();
-          if(!this.handled401) {
-            this.verboseDetails(req, error)
-            this.handled401 = true;
-            return this.router.navigate(['/restoreSession']);
-          }
-          return this.router.navigate(['/unauthorize']);
+        if (this.shouldHandleUnauthorized(error)) {
+          return this.handleUnauthorizedError(req, error);
         }
 
         // Handle timeouts
-        if (error.status === 200 && error.url?.startsWith('https://auth')) {
-          if(this.debugMode) this.logger.debug('Timeout encountered - redirect to login.', error);
-          this.heartbeatService.pause();
-          let url = '/fs/' + this.location.prepareExternalUrl(this.router.serializeUrl(this.router.createUrlTree(['restoreSession'])));
-          url = window.location.origin + url;
-
-          const features = 'popup,menubar=yes,scrollbars=yes,resizable=yes,width=850,height=700,noreferrer';
-
-          const errorUrl = new URL(error.url);
-          errorUrl.searchParams.delete('TARGET');
-          errorUrl.searchParams.set('TARGET', url);
-
-          if (!this.modalWindow) {
-            this.modalWindow = openNewWindow(errorUrl.toString(), 'Restore_Session', features);
-          }
-
-          return of(undefined);
+        if (this.shouldHandleTimeout(error)) {
+          return this.handleTimeout(error);
         } else {
-          this.verboseDetails(req, error);
-          const timestamp: number = Date.now();
-          this.errorHandler.registerNewError(timestamp, error);
-          this.router.navigate(['/error', timestamp]).then(() => {
-            this.logger.error('Finished navigating to /error');
-          });
-          return throwError(error);
+          return this.handleAllOtherErrors(req, error);
         }
-      }), finalize(() => {
+      }),
+      finalize(() => {
         this.modalWindow = undefined;
       })
     );
   }
 
+  private handleUnauthorizedError(req: HttpRequest<any>, error) {
+    this.heartbeatService.pause();
+    if (!this.handled401) {
+      this.verboseDetails(req, error);
+      this.handled401 = true;
+      return this.router.navigate(['/unauthorize']);
+    }
+    return of(EMPTY);
+  }
+
+  private shouldHandleUnauthorized(error) {
+    return error.status === 401;
+  }
+
+  private shouldHandleTimeout(error) {
+    return error.status === 200 && error.url?.startsWith('https://auth');
+  }
+
+  private handleAllOtherErrors(req: HttpRequest<any>, error) {
+    // Handle all other errors
+    this.verboseDetails(req, error);
+    const timestamp: number = Date.now();
+    this.errorHandler.registerNewError(timestamp, error);
+    this.router.navigate(['/error', timestamp]).then(() => {
+      this.logger.error('Finished navigating to /error');
+    });
+    return throwError(error);
+  }
+
+  private handleTimeout(error: any) {
+    if (this.debugMode) this.logger.debug('Timeout encountered - redirect to login.');
+    this.heartbeatService.pause();
+    let url =
+      '/fs/' +
+      this.location.prepareExternalUrl(this.router.serializeUrl(this.router.createUrlTree(['restoreSession'])));
+    url = window.location.origin + url;
+
+    const features = 'popup,menubar=yes,scrollbars=yes,resizable=yes,width=850,height=700,noreferrer';
+
+    const errorUrl = new URL(error.url);
+    errorUrl.searchParams.delete('TARGET');
+    errorUrl.searchParams.set('TARGET', url);
+
+    if (!this.modalWindow) {
+      this.modalWindow = openNewWindow(errorUrl.toString(), 'Restore_Session', features);
+    }
+
+    return of(undefined);
+  }
+
   handleNavigationStart(event: NavigationStart): void {
-    if(this.debugMode) this.logger.debug('=======> NavigationStart', event);
+    if (this.debugMode) this.logger.debug('=======> NavigationStart', event);
   }
 
   handleNavigationEnd(event: NavigationEnd): void {
-    if(this.debugMode) this.logger.debug('=======> NavigationEnd', event);
+    if (this.debugMode) this.logger.debug('=======> NavigationEnd', event);
   }
 
   private verboseDetails(req: HttpRequest<any>, error: any) {
