107f933033c5d8edd55fa1e5edab211d0cdedd77
Doug Kanoza <kanozad@users.noreply.github.com>
2026-06-23 10:09:16 -0400

    Fix dev auth redirect URI path

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -17,7 +17,7 @@ export const environment = {
     auth: {
       clientId: '4450b02b-ea0d-4a8c-bf01-cbad66fb841c',
       authority: 'https://login.microsoftonline.com/14b77578-9773-42d5-8507-251ca2dc2b06',
-      redirectUri: 'fs',
+      redirectUri: '/fs/',
     }
   },
   apiConfig: {
